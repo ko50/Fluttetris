b931acce6336a1a132468f591f0aeae121f9908b
ko5 <kotyana1331@gmail.com>
2020-10-18 00:00:26 +0900

    [追加] ミノを回転できるようにした (まだ変化するのは配置情報だけ)

--- lib/system/tetromino/mino_placement.dart
+++ lib/system/tetromino/mino_placement.dart
@@ -1,5 +1,30 @@
+import 'package:flute_tris/system/tetromino/enum/mino_type.dart';
+import 'package:flute_tris/system/common/rotate_direction.dart';
+
 class MinoPlacement {
   List<List<int>> currentPlacement;
+
+  void rotate(RotateDirection direction, TetroMino minoType) {
+    if (minoType == TetroMino.O) return;
+
+    List<List<int>> beforePlacement = List.from(
+            direction == RotateDirection.Right
+                ? currentPlacement.reversed.toList()
+                : currentPlacement),
+        rotatedPlacement = [[], [], []];
+    if (minoType == TetroMino.I) rotatedPlacement.add([]);
+
+    beforePlacement.asMap().forEach((horizonIndex, row) {
+      row.asMap().forEach((blockIndex, _) => rotatedPlacement[horizonIndex]
+          .add(beforePlacement[blockIndex][horizonIndex]));
+    });
+
+    // TODO SRS繁栄
+
+    currentPlacement = direction == RotateDirection.Right
+        ? rotatedPlacement
+        : rotatedPlacement.reversed.toList();
+  }
 
   MinoPlacement(this.currentPlacement);
 }
