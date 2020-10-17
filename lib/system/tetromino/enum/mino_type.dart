c1ed64139021338965e8c3b65cbc7f9caeb30e2b
ko5 <kotyana1331@gmail.com>
2020-10-17 23:42:26 +0900

    [追加] テトリミノの種類とそれのデフォルトのブロック配置を追加

---
+++ lib/system/tetromino/enum/mino_type.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+enum TetroMino { T, O, S, Z, L, J, I }
+
+extension MinoPlacements on TetroMino {
+  List<List<int>> get defaultPlacement {
+    switch (this) {
+      case TetroMino.T:
+        return [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ];
+      case TetroMino.O:
+        return [
+          [1, 1, 0],
+          [1, 1, 0],
+          [0, 0, 0],
+        ];
+      case TetroMino.S:
+        return [
+          [0, 1, 1],
+          [1, 1, 0],
+          [0, 0, 0],
+        ];
+      case TetroMino.Z:
+        return [
+          [1, 1, 0],
+          [0, 1, 1],
+          [0, 0, 0],
+        ];
+      case TetroMino.L:
+        return [
+          [0, 0, 1],
+          [1, 1, 1],
+          [0, 0, 0],
+        ];
+      case TetroMino.J:
+        return [
+          [1, 0, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ];
+      case TetroMino.I:
+        return [
+          [0, 0, 0, 0],
+          [1, 1, 1, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ];
+      default:
+        return null;
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case TetroMino.T:
+        return Colors.purple;
+      case TetroMino.O:
+        return Colors.yellow;
+      case TetroMino.S:
+        return Colors.lightGreen;
+      case TetroMino.Z:
+        return Colors.red;
+      case TetroMino.L:
+        return Colors.orange;
+      case TetroMino.J:
+        return Colors.blue[700];
+      case TetroMino.I:
+        return Colors.cyan;
+
+      default:
+        return null;
+    }
+  }
+}
