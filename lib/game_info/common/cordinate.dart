d512023d798cd8db3ea2148305ea794e1b5dc225
ko5 <kotyana1331@gmail.com>
2020-11-21 16:00:59 +0900

    [変更] 重複判定を取れる関数を修正した

---
+++ lib/game_info/common/cordinate.dart
@@ -0,0 +1,26 @@
+class Cordinate {
+  int x;
+  int y;
+
+  Cordinate(this.x, this.y);
+
+  String toString() => "($x, $y)";
+
+  Cordinate operator +(Cordinate other) => Cordinate(x + other.x, y + other.y);
+
+  Cordinate operator -(Cordinate other) => Cordinate(x + other.x, y - other.y);
+
+  bool operator ==(dynamic? cordinate) =>
+      cordinate?.x == x && cordinate?.y == y;
+
+  @override
+  int get hashCode => super.hashCode;
+
+  bool isStucking(Cordinate other) => other.x == x || other.y == y;
+
+  void down() => y -= 1;
+
+  void toRight() => x += 1;
+
+  void toLeft() => x -= 1;
+}
---
+++ lib/game_info/common/placed_blocks.dart
@@ -0,0 +1,21 @@
+import 'package:flute_tris/game_info/common/cordinate.dart';
+import 'package:flute_tris/game_info/render/block.dart';
+
+class PlacedBlocks {
+  static final List<List<Block>> placedBlocks = [];
+
+  static List<List<Cordinate>> get allCordinates => placedBlocks
+      .map((row) => row.map((block) => block.cordinate).toList())
+      .toList();
+
+  bool isOverlappingWith(List<Cordinate> pendingCordinates) {
+    final bool isOverlapping = allCordinates.any(
+      (row) => row.any(
+        (cordinate) => pendingCordinates
+            .any((pendingCordinate) => pendingCordinate.isStucking(cordinate)),
+      ),
+    );
+
+    return isOverlapping;
+  }
+}
