fcfe04c0b64436c8261a9e26c4f6e6df9316215f
ko5 <kotyana1331@gmail.com>
2020-11-23 17:44:45 +0900

    [追加] フィールドを定義

---
+++ lib/game_info/game_field/game_field.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flute_tris/game_info/enum/move_direction.dart';
+import 'package:flute_tris/game_info/operation_mino/operation_mino.dart';
+
+class GameField {
+  OperationMino operationMino;
+  bool pause = true;
+  late final Timer timer;
+
+  GameField({required this.operationMino}) {
+    timer = Timer.periodic(Duration(milliseconds: 500), (_) {
+      if (!pause) operationMino.move(MoveDirection.Down);
+    });
+  }
+}
