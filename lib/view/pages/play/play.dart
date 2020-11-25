70e1c4d69af8ffc65ed62a0a8d9f946840fa6ca6
ko <60608740+ko50@users.noreply.github.com>
2020-11-25 20:17:47 +0900

    フロントのファイルを作成した (#6)
    * [修正] main.dartを最小化
    
    * [追加] まだ書いてないけどファイルだけ追加

---
+++ lib/view/pages/play/play.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Play extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/view/widgets/buttons/mode_select_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ModeSelectButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Colors.grey,
+      onPressed: () {},
+    );
+  }
+}
