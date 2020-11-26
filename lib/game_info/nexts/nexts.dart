3454c345f73115ce764c08e00fc7c56b8b08aa34
ko5 <kotyana1331@gmail.com>
2020-11-26 20:49:40 +0900

    [追加] Nextsを追加、supplyNextsを実装

---
+++ lib/game_info/nexts/nexts.dart
@@ -0,0 +1,12 @@
+import 'package:flute_tris/game_info/enum/mino_type.dart';
+
+class Nexts {
+  static List<TetroMino> nexts = TetroMino.values..shuffle();
+  static late TetroMino nextMino = nexts[0];
+
+  static void supplyNexts() {
+    if (nexts.length >= 6) return;
+
+    nexts.addAll(TetroMino.values..shuffle());
+  }
+}
