65a284b356694b61876d7d5ff0a8a72dfb8afa06
ko5 <kotyana1331@gmail.com>
2020-11-30 01:24:44 +0900

    [変更] OperationModel

--- lib/bloc/operation_model.dart
+++ lib/bloc/operation_model.dart
@@ -1,45 +1,31 @@
-import 'dart:async';
+import 'package:flutter/widgets.dart';
 
-import 'package:flute_tris/game_info/common/cordinate.dart';
 import 'package:flute_tris/game_info/common/placed_blocks.dart';
 import 'package:flute_tris/game_info/enum/mino_type.dart';
 import 'package:flute_tris/game_info/enum/move_direction.dart';
 import 'package:flute_tris/game_info/enum/rotate_direction.dart';
 import 'package:flute_tris/game_info/operation_mino/operation_mino.dart';
 
-class OperationBloc {
+class OperationModel extends ChangeNotifier {
   OperationMino operationMino;
 
-  OperationBloc(TetroMino minoType)
+  OperationModel(TetroMino minoType)
       : operationMino = OperationMino(minoType: minoType);
-
-  final StreamController<List<Cordinate>> _minoCordinateController =
-      StreamController<List<Cordinate>>();
-
-  Stream<List<Cordinate>> get minoCordinateStream =>
-      _minoCordinateController.stream;
-
-  void _updateCordinate() =>
-      _minoCordinateController.sink.add(operationMino.location.currentLocation);
 
   void move(MoveDirection direction) {
     operationMino.move(direction);
-    _updateCordinate();
+    notifyListeners();
   }
 
   void rotate(RotateDirection direction) {
     operationMino.rotate(direction);
-    _updateCordinate();
+    notifyListeners();
   }
 
   void put(TetroMino next) {
     PlacedBlocks.placedBlocks.addAll(operationMino.blocks);
     operationMino = OperationMino(minoType: next);
 
-    _updateCordinate();
-  }
-
-  void dispose() {
-    _minoCordinateController.close();
+    notifyListeners();
   }
 }
