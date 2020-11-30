43d919f288f1394db53bbbacfc94ca0779af7d8f
ko5 <kotyana1331@gmail.com>
2020-11-30 19:23:03 +0900

    [変更] 先祖から受け継いだブロックを描画するようにした

--- lib/view/widgets/displayer/disp_blocks.dart
+++ lib/view/widgets/displayer/disp_blocks.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flute_tris/game_info/render/block.dart';
+import 'package:flute_tris/models/operation_model.dart';
+import 'package:flute_tris/models/placed_blocks_model.dart';
+import 'package:flute_tris/view/widgets/tetromino/tetris_block.dart';
 
 class BlockDisplayer extends StatelessWidget {
   final int height;
@@ -31,11 +37,20 @@ class BlockDisplayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Block> placedBlocks =
+            Provider.of<PlacedBlocksModel>(context).placedBlocks,
+        operationBlocks =
+            Provider.of<OperationModel>(context).operationMino.blocks;
+
+    final List<Widget> wholeBlocks = (placedBlocks + operationBlocks)
+        .map((b) => TetrisBlock(block: b, gridSize: gridSize))
+        .toList();
+
     return Container(
       height: height * gridSize,
       width: width * gridSize,
       decoration: BoxDecoration(boxShadow: [BoxShadow()]),
-      child: Stack(children: [_background()] + children),
+      child: Stack(children: [_background()] + wholeBlocks),
     );
   }
 }
