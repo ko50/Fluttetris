d4e5ada45203552eabc119a4dd91c94977630ea3
ko <60608740+ko50@users.noreply.github.com>
2020-11-30 23:21:22 +0900

    [追加] Play画面を追加 (#27)
    * [修正] childrenは必要ない
    
    * [追加] Play画面を追加

--- lib/view/pages/play/play.dart
+++ lib/view/pages/play/play.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flute_tris/game_info/enum/mino_type.dart';
+import 'package:flute_tris/models/hold_model.dart';
+import 'package:flute_tris/models/nexts_model.dart';
+import 'package:flute_tris/models/operation_model.dart';
+import 'package:flute_tris/models/placed_blocks_model.dart';
+import 'package:flute_tris/view/widgets/displayer/disp_blocks.dart';
+import 'package:flute_tris/view/widgets/displayer/disp_holded.dart';
+import 'package:flute_tris/view/widgets/displayer/disp_nexts.dart';
 
 class Play extends StatelessWidget {
+  Widget _displays() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          HoldedMinoDisplayer(),
+          BlockDisplayer(height: 21, width: 10),
+          NextsDisplayer(),
+        ],
+      ),
+    );
+  }
+
+  // Widget _operators() {}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Play")),
+      body: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<HoldModel>(create: (_) => HoldModel()),
+          ChangeNotifierProvider<NextsModel>(create: (_) => NextsModel()),
+          ChangeNotifierProvider<PlacedBlocksModel>(
+            create: (_) => PlacedBlocksModel(),
+          ),
+        ],
+        builder: (BuildContext context, Widget child) {
+          final TetroMino firstMino =
+              Provider.of<NextsModel>(context, listen: false).nextMino;
+
+          return ChangeNotifierProvider<OperationModel>(
+            create: (_) => OperationModel(firstMino),
+            child: child,
+          );
+        },
+        child: Container(
+          child: Column(
+            children: [_displays()],
+          ),
+        ),
+      ),
+    );
   }
 }
