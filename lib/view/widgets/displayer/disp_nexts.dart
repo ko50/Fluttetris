c6bdfc9f11c3d968802703e8ea344863d54d3b49
ko5 <kotyana1331@gmail.com>
2020-11-30 20:41:19 +0900

    [追加] ネクストを表示するウィジェットを追加

---
+++ lib/view/widgets/displayer/disp_nexts.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flute_tris/game_info/enum/mino_type.dart';
+import 'package:flute_tris/models/nexts_model.dart';
+import 'package:flute_tris/view/widgets/displayer/tetromino_displayer.dart';
+
+class NextsDisplayer extends StatelessWidget {
+  Widget _recent(TetroMino mino) => TetroMinoDisplayer(
+        minoType: mino,
+        gridSize: 30,
+        scale: 1,
+      );
+
+  Widget _sub(TetroMino mino) => Padding(
+      padding: EdgeInsets.only(bottom: 8.0),
+      child: TetroMinoDisplayer(
+        minoType: mino,
+        gridSize: 30,
+        scale: 0.8,
+      ));
+
+  List<Widget> _displayers(List<TetroMino> nexts) {
+    final List<Widget> displayers = [];
+
+    displayers.add(_recent(nexts.first));
+    displayers.addAll(nexts.sublist(1).map((mino) => _sub(mino)).toList());
+
+    return displayers;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<TetroMino> nextMinos = Provider.of<NextsModel>(context).nexts;
+
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: _displayers(nextMinos),
+      ),
+    );
+  }
+}
